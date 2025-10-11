072d5201a5e9c0f26fb167931ff3d49b00fb386e
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-11 18:44:24 +0200

    Bootstrap the repository in the GithubStore constructor

--- src/main/java/com/recceda/core/store/github/GithubOtpStore.java
+++ src/main/java/com/recceda/core/store/github/GithubOtpStore.java
@@ -1,18 +1,27 @@
 package com.recceda.core.store.github;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.recceda.action.RepositoryAction;
 import com.recceda.core.store.OtpStore;
 import com.recceda.core.store.reccedda.ReccedaOtpStore;
+import com.recceda.elements.Repository;
 import com.recceda.http.github.GithubClient;
+import com.recceda.http.requests.repository.CreateRepositoryRequest;
+
+import java.util.concurrent.ExecutionException;
 
 public class GithubOtpStore implements OtpStore {
 
     private final GithubClient githubClient;
     private final RepositoryAction repositoryAction;
+    private final Repository repository;
+    private final String otpStoreName;
 
-    public GithubOtpStore(String githubToken) {
+    public GithubOtpStore(String githubToken, String otpStoreName) throws ExecutionException, InterruptedException, JsonProcessingException {
+        this.otpStoreName = otpStoreName;
         this.githubClient = new GithubClient(githubToken);
         this.repositoryAction = new RepositoryAction(this.githubClient);
+        this.repository = this.repositoryAction.createRepositoryForAuthenticatedUser(this.createOtpRepository(otpStoreName));
     }
 
     @Override
@@ -35,9 +44,19 @@ public class GithubOtpStore implements OtpStore {
 
     }
 
+    private CreateRepositoryRequest createOtpRepository(String otpStoreName) {
+        return CreateRepositoryRequest.builder()
+                .description("Recceda Otp Repository")
+                .name(this.otpStoreName)
+                .isPrivate(false)
+                .isTemplate(false)
+                .build();
+    }
+
     public static class OtpEntry {
+        public long expiryTime;
+        public int failedAttempts;
         private String otp;
         private String key;
-        public long expiryTime;
-        public int failedAttempts;    }
+    }
 }
