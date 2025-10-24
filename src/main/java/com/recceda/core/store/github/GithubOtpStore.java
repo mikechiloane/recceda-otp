36ceebd1cf173bf1a5bc8290cfb131d32019cc54
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-24 19:25:34 +0200

    complete the store

--- src/main/java/com/recceda/core/store/github/GithubOtpStore.java
+++ src/main/java/com/recceda/core/store/github/GithubOtpStore.java
@@ -9,6 +9,7 @@ import com.recceda.elements.Committer;
 import com.recceda.elements.Repository;
 import com.recceda.http.github.GithubClient;
 import com.recceda.http.requests.file.CreateFileRequest;
+import com.recceda.http.requests.file.DeleteFileRequest;
 import com.recceda.http.requests.repository.CreateRepositoryRequest;
 import lombok.AllArgsConstructor;
 
@@ -58,6 +59,13 @@ public class GithubOtpStore implements OtpStore {
 
     @Override
     public void invalidateOtp(String key) {
+        try {
+            String sha = fileAction.getFileContents(this.repository.getOwner().getLogin(), this.repository.getName(), this.generateOtpFileName(key)).getSha();
+
+            fileAction.deleteFile(this.createDeleteFileRequest(key, sha), this.repository.getOwner().getLogin(), this.repository.getName(), this.generateOtpFileName(key));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private CreateRepositoryRequest createOtpRepository(String otpStoreName) {
@@ -69,6 +77,7 @@ public class GithubOtpStore implements OtpStore {
                 .build();
     }
 
+
     private CreateFileRequest createFileRequestForOtp(OtpEntry otpEntry) throws JsonProcessingException {
         String message = LocalDate.now() + " " + this.otpStoreName + " " + otpEntry.key;
         Committer committer = Committer.builder()
@@ -76,6 +85,17 @@ public class GithubOtpStore implements OtpStore {
                 .email("test@example.com").build();
 
         return new CreateFileRequest(otpEntry, committer, message);
+    }
+
+    private DeleteFileRequest createDeleteFileRequest(String key, String sha) throws JsonProcessingException {
+        String message = LocalDate.now() + " " + this.otpStoreName + " " + key
+                + " deleted";
+        Committer committer = Committer.builder()
+                .name(this.getClass().getName())
+                .email("test@email.com")
+                .build();
+
+        return DeleteFileRequest.builder().sha(sha).message(message).committer(committer).build();
     }
 
     private String generateOtpFileName(String key) {
